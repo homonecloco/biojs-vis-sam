20d752b7cafb25f6fba5b7aeeb5df28a33fa9f99
Ricardo H. Ramírez-Gonzalez <ricardo.ramirez-gonzalez@tgac.ac.uk>
2014-11-28 06:44:06 -0600

    minor improvement in the snipper

--- snippets/single.js
+++ snippets/single.js
@@ -1,5 +1,5 @@
 var sam = require("biojs-vis-sam");
-var bw = 10;
+var bw = 5;
 var flanking_cache = 50;
 
 var instance = new sam.BAMViewer({
@@ -9,9 +9,10 @@ var instance = new sam.BAMViewer({
   base_width: bw,
   flanking_cache: flanking_cache ,
   default_region: "chr_1:1-404",
-  height: "500px",
-  //width:"1000px",         
-  display_orientation: true
+  height: "200px",
+  width:"800px",         
+  display_orientation: true,
+  display_bases: true
 }); 
 console.log(instance);
 instance.load_default_region(); 
